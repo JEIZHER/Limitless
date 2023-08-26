d3387b189cb6e79d4a8d0926c2771b108b56f0dc
Joseph Izarra <new.jeizher@gmail.com>
2023-08-26 14:49:41 -0400

    prueba

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,15 @@
 function Home() {
 	return (
 		<>
-			<div className="flex flex-col gap-1 sm:flex-row  w-full h-screen">
-				{/* <h1 className='text-sm sm:text-lg lg:text-2xl font-bold underline '>Home</h1> */}
-				<div className="border border-sky-700 rounded-md h-1/2 w-full sm:h-full sm:w-1/2"></div>
-				<div className="border border-sky-700 rounded-md h-1/2 w-full sm:h-full sm:w-1/2"></div>
+			<h1 className='mt-11 p-1 text-sm font-semibold underline '>Home</h1>
+			<div className="flex flex-col gap-1  w-full h-screen sm:flex-row  ">
+				
+				<div className=" border border-sky-700 rounded-md h-1/2 w-full sm:h-full sm:w-1/2">
+
+				</div>
+				<div className="border border-sky-700 rounded-md h-1/2 w-full sm:h-full sm:w-1/2">
+
+				</div>
 			</div>
 		</>
 	)
