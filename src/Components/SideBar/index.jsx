9d21353c1915210c5765bebc337e93617071204e
Joseph Izarra <new.jeizher@gmail.com>
2023-09-08 18:06:36 -0400

    colocada informacion de cards en arreglos y  leidas y renderizadas con map

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -8,23 +8,23 @@ const Sidebar = () => {
 
 		<nav className=' hidden fixed top-0 h-12 lg:h-full  w-full lg:w-[250px] z-1000 bg-blue-900 sm:flex flex-row lg:flex-col justify-center items-center text-white text-bold text-[11px] '>
 			<ul className='menu flex flex-row lg:flex-col gap-3'>
-				<li className='w-20 hover:ring-4'>
-					<a href='#intro' className={`underline-offset-4   ${activeStyle}`}>
+				<li className='w-30 hover:ring-4'>
+					<a href='#intro' className={`tracking-[2.2px] underline-offset-4   ${activeStyle}`}>
 						WELCOME
 					</a>
 				</li >
-				<li  className='w-20 hover:ring-4'>
-					<a href='#one' className='underline-offset-4'>
+				<li  className='w-30 hover:ring-4'>
+					<a href='#one' className='tracking-[2.2px] underline-offset-4'>
 						WHO WE ARE
 					</a>
 				</li>
-				<li  className='w-20 hover:ring-4'>
-					<a href='#two' className='underline-offset-4'>
+				<li  className='w-30 hover:ring-4'>
+					<a href='#two' className='tracking-[2.2px] underline-offset-4'>
 						WHAT WE DO
 					</a>
 				</li>
-				<li>
-					<a href='#three' className='underline-offset-4'>
+				<li className='w-30 hover:ring-4'>
+					<a href='#three' className='tracking-[2.2px] underline-offset-4'>
 						GET IN TOUCH
 					</a>
 				</li>
